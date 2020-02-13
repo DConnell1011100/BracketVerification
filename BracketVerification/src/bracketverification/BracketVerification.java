7287e8f93630ff4acbfe66d7cced0a3493f588b5
DConnell1011100 <DConnell1011100@users.noreply.github.com>
2020-02-13 11:15:46 +0000

    Added comments

--- BracketVerification/src/bracketverification/BracketVerification.java
+++ BracketVerification/src/bracketverification/BracketVerification.java
@@ -15,23 +15,23 @@ public class BracketVerification {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Stack<Character> stack = new Stack<>();
-        String str = "{]}";
-        boolean validBracket = true;
+        Stack<Character> stack = new Stack<>(); //declare new Stack
+        String str = "{()()(/)()}"; //declare and initialize a string of brackets
+        boolean validBracket = true; // declare and initialize a boolean to return if brakcets are valid
         
-        for(int i = 0; i < str.length(); i++){
+        for(int i = 0; i < str.length(); i++){//a for loop to iterate over the string to push to the stack
             stack.push(str.charAt(i));
         }
         
-        for(int i = 0; i < str.length(); i++){
-            char strCurrent = str.charAt(i);
-            char stackCurrent = stack.peek();
+        for(int i = 0; i < str.length(); i++){//a for loop to iterate the string
+            char strCurrent = str.charAt(i);//set strCurrent as the current character of the String
+            char stackCurrent = stack.peek();//set stackCurrent as the top character without removing it
             if(strCurrent == '{' && stackCurrent == '}' || (strCurrent == '}' && stackCurrent == '{')){
-                validBracket = true;
-                stack.pop();
+                validBracket = true;//if the brackets in str and stack are mirrors, then it is valide and moves on
+                stack.pop();// if it is valide, we pop the top char so that we can check the next one
             }
             else if(strCurrent == '[' && stackCurrent == ']' || (strCurrent == ']' && stackCurrent == '[')){
-                validBracket = true;
+                validBracket = true;//same as before but with different bracket type
                 stack.pop();
             }
             else if(strCurrent == '(' && stackCurrent == ')' || (strCurrent == ')' && stackCurrent == '(')){
@@ -39,11 +39,11 @@ public class BracketVerification {
                 stack.pop();
             }
             else if(stack.isEmpty()){
-                validBracket = true;
+                validBracket = true;//without this the validBracket returns false becuase there is no matching brackets - this occurs once all stack is removed
                 stack.pop();
             }
             else{
-              validBracket = false;
+              validBracket = false;//if there is no mirror in the brackets, it is false
               break;
             }
         }
