97f62dd4cc7e317a8579aca3f74e9fd6190d415f
DConnell1011100 <DConnell1011100@users.noreply.github.com>
2020-02-13 11:05:31 +0000

    Working solution to be broken
    Method works but needs to be tested and broken

--- BracketVerification/src/bracketverification/BracketVerification.java
+++ BracketVerification/src/bracketverification/BracketVerification.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package bracketverification;
-
+import java.util.Stack;
 /**
  *
  * @author danco
@@ -15,7 +15,38 @@ public class BracketVerification {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Stack<Character> stack = new Stack<>();
+        String str = "{]}";
+        boolean validBracket = true;
+        
+        for(int i = 0; i < str.length(); i++){
+            stack.push(str.charAt(i));
+        }
+        
+        for(int i = 0; i < str.length(); i++){
+            char strCurrent = str.charAt(i);
+            char stackCurrent = stack.peek();
+            if(strCurrent == '{' && stackCurrent == '}' || (strCurrent == '}' && stackCurrent == '{')){
+                validBracket = true;
+                stack.pop();
+            }
+            else if(strCurrent == '[' && stackCurrent == ']' || (strCurrent == ']' && stackCurrent == '[')){
+                validBracket = true;
+                stack.pop();
+            }
+            else if(strCurrent == '(' && stackCurrent == ')' || (strCurrent == ')' && stackCurrent == '(')){
+                validBracket = true;
+                stack.pop();
+            }
+            else if(stack.isEmpty()){
+                validBracket = true;
+                stack.pop();
+            }
+            else{
+              validBracket = false;
+              break;
+            }
+        }
+        System.out.println("Brackets Valid:" +validBracket);
     }
-    
 }
